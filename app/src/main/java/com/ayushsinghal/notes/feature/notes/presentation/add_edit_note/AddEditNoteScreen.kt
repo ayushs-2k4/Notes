b402c50c7cebfb654136ec01ad86a60bcebbcec4
Ayush Singhal <ayushsinghals02@gmail.com>
2023-06-19 23:23:45 +0530

    Changed Plus Icon in Tag Row

--- app/src/main/java/com/ayushsinghal/notes/feature/notes/presentation/add_edit_note/AddEditNoteScreen.kt
+++ app/src/main/java/com/ayushsinghal/notes/feature/notes/presentation/add_edit_note/AddEditNoteScreen.kt
@@ -3,7 +3,6 @@ package com.ayushsinghal.notes.feature.notes.presentation.add_edit_note
 import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,13 +15,10 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Restore
 import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.AddBox
 import androidx.compose.material.icons.outlined.Archive
-import androidx.compose.material.icons.outlined.Delete
-import androidx.compose.material.icons.outlined.DeleteForever
 import androidx.compose.material.icons.outlined.Unarchive
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.BottomAppBar
@@ -31,7 +27,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -46,13 +41,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.FocusState
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -451,7 +442,7 @@ fun TagsColumn(
 
             item {
                 IconButton(onClick = { onAddTagButtonClick() }) {
-                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add Tag")
+                    Icon(imageVector = Icons.Outlined.AddBox, contentDescription = "Add Tag")
                 }
             }
         }
